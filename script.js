7b55fa5495dc5c217eb662dfb726398a28c1c525
hongbowu <wuhongbo87@gmail.com>
2023-11-27 23:30:10 -0500

    add stater code

---
+++ script.js
@@ -0,0 +1,13 @@
+//need add a timer.
+
+//need add quiz body
+
+//add function for answer questions
+
+//add feedback for correct or wrong.
+
+//count the scores
+
+//add local storage for scores
+
+//add function for show up high scores.
